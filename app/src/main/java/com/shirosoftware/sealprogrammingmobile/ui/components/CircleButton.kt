99b3c8cbd14d214b94ca30fcaf40058e825cbe8e
Omoti <guti.tomtom@gmail.com>
2022-08-19 19:29:29 +0900

    add:buttons

---
+++ app/src/main/java/com/shirosoftware/sealprogrammingmobile/ui/components/CircleButton.kt
@@ -0,0 +1,58 @@
+package com.shirosoftware.sealprogrammingmobile.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Camera
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.shirosoftware.sealprogrammingmobile.ui.theme.SealProgrammingMobileTheme
+
+@Composable
+fun CircleButton(
+    icon: ImageVector,
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Button(
+        shape = CircleShape, onClick = onClick, modifier = modifier
+            .width(72.dp)
+            .height(72.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                icon,
+                contentDescription = text,
+                modifier
+                    .width(24.dp)
+                    .height(24.dp)
+            )
+            Spacer(modifier = Modifier.height(2.dp))
+            Text(text = text, fontSize = 12.sp)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CircleButtonPreview() {
+    SealProgrammingMobileTheme {
+        CircleButton(icon = Icons.Default.Camera, text = "カメラ", onClick = { /*TODO*/ })
+    }
+}
--- app/src/main/java/com/shirosoftware/sealprogrammingmobile/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/shirosoftware/sealprogrammingmobile/ui/screens/main/MainScreen.kt
@@ -1,9 +1,60 @@
 package com.shirosoftware.sealprogrammingmobile.ui.screens.main
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bluetooth
+import androidx.compose.material.icons.filled.ElectricCar
+import androidx.compose.material.icons.filled.PhotoCamera
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.shirosoftware.sealprogrammingmobile.R
+import com.shirosoftware.sealprogrammingmobile.ui.components.CircleButton
+import com.shirosoftware.sealprogrammingmobile.ui.theme.SealProgrammingMobileTheme
 
 @Composable
-fun MainScreen() {
-    Text(text = "main")
+fun MainScreen(modifier: Modifier = Modifier) {
+    Scaffold(topBar = {
+        TopAppBar(title = { Text(text = stringResource(id = R.string.app_name)) })
+    }) {
+        Column(verticalArrangement = Arrangement.Bottom, modifier = modifier.fillMaxSize()) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
+            ) {
+                CircleButton(
+                    Icons.Default.PhotoCamera,
+                    stringResource(id = R.string.main_button_camera),
+                    onClick = { /*TODO*/ })
+                CircleButton(
+                    Icons.Default.Bluetooth,
+                    stringResource(id = R.string.main_button_connect),
+                    onClick = { /*TODO*/ })
+                CircleButton(
+                    Icons.Default.ElectricCar,
+                    stringResource(id = R.string.main_button_send),
+                    onClick = { /*TODO*/ })
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainScreenPreview() {
+    SealProgrammingMobileTheme {
+        MainScreen()
+    }
 }
