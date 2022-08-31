19b9e327bc02a7e732d584b87e404a11f672feac
Omoti <guti.tomtom@gmail.com>
2022-08-31 20:35:08 +0900

    feat:add camera preview

--- app/src/main/java/com/shirosoftware/sealprogrammingmobile/ui/screens/camera/CameraScreen.kt
+++ app/src/main/java/com/shirosoftware/sealprogrammingmobile/ui/screens/camera/CameraScreen.kt
@@ -1,8 +1,18 @@
 package com.shirosoftware.sealprogrammingmobile.ui.screens.camera
 
-import androidx.compose.material3.Text
+import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
 
 @Composable
 fun CameraScreen(
@@ -10,5 +20,52 @@ fun CameraScreen(
     modifier: Modifier = Modifier,
     onBack: () -> Unit = {},
 ) {
-    Text(text = "Camera")
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    AndroidView(
+        modifier = modifier
+            .fillMaxSize()
+            .aspectRatio(3.0f / 4.0f),
+        factory = { context ->
+
+            // プレビュー
+            // https://developer.android.com/training/camerax/preview?hl=ja
+            val scaleType = PreviewView.ScaleType.FILL_CENTER
+
+            val previewView = PreviewView(context).apply {
+                this.scaleType = scaleType
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                )
+                implementationMode = PreviewView.ImplementationMode.PERFORMANCE
+            }
+
+            val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+
+            cameraProviderFuture.addListener({
+                val cameraProvider = cameraProviderFuture.get()
+                bindPreview(previewView, cameraProvider, lifecycleOwner)
+            }, ContextCompat.getMainExecutor(context))
+
+            previewView
+        },
+    )
+}
+
+fun bindPreview(
+    previewView: PreviewView,
+    cameraProvider: ProcessCameraProvider,
+    lifecycleOwner: LifecycleOwner,
+) {
+    val preview: Preview = Preview.Builder()
+        .build()
+
+    val cameraSelector: CameraSelector = CameraSelector.Builder()
+        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+        .build()
+
+    preview.setSurfaceProvider(previewView.surfaceProvider)
+
+    var camera = cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview)
 }
