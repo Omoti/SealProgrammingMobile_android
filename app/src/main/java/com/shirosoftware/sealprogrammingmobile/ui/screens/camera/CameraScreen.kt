ea6f0ae168d36b3caef267fe1ab965944ae4f675
Omoti <guti.tomtom@gmail.com>
2022-09-02 07:52:30 +0900

    feat:button layout

--- app/src/main/java/com/shirosoftware/sealprogrammingmobile/ui/screens/camera/CameraScreen.kt
+++ app/src/main/java/com/shirosoftware/sealprogrammingmobile/ui/screens/camera/CameraScreen.kt
@@ -1,18 +1,16 @@
 package com.shirosoftware.sealprogrammingmobile.ui.screens.camera
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun CameraScreen(
@@ -26,16 +24,26 @@ fun CameraScreen(
         // プレビュー
         CameraPreview(lifecycleOwner = lifecycleOwner, modifier = Modifier.fillMaxWidth())
 
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
+        ConstraintLayout(
             modifier = Modifier.fillMaxSize()
         ) {
+            val (shutterButton, switchButton) = createRefs()
+
             ShutterButton(
-                modifier = Modifier.size(88.dp),
+                modifier = Modifier
+                    .size(88.dp)
+                    .constrainAs(shutterButton) {
+                        centerTo(parent)
+                    },
             )
             SwitchCameraButton(
-                modifier = Modifier.size(64.dp),
+                modifier = Modifier
+                    .size(56.dp)
+                    .constrainAs(switchButton) {
+                        centerVerticallyTo(parent)
+                        start.linkTo(shutterButton.end)
+                        end.linkTo(parent.end)
+                    },
             )
         }
     }
