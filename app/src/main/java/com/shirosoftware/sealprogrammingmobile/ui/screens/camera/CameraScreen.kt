a68d3c819ed30040825ee826cb046afd455e9db5
Omoti <guti.tomtom@gmail.com>
2022-09-03 09:43:07 +0900

    feat:save a picture

--- app/src/main/java/com/shirosoftware/sealprogrammingmobile/ui/screens/camera/CameraScreen.kt
+++ app/src/main/java/com/shirosoftware/sealprogrammingmobile/ui/screens/camera/CameraScreen.kt
@@ -1,16 +1,32 @@
 package com.shirosoftware.sealprogrammingmobile.ui.screens.camera
 
+import android.content.Context
+import android.os.Environment
+import android.util.Log
+import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.core.content.ContextCompat
+import java.io.File
 
 @Composable
 fun CameraScreen(
@@ -19,10 +35,65 @@ fun CameraScreen(
     onBack: () -> Unit = {},
 ) {
     val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
+    val configuration = LocalConfiguration.current
+
+    val imageCapture = ImageCapture.Builder()
+        .setTargetRotation(configuration.orientation)
+        .build()
 
     Column(modifier = modifier.background(Color.Black)) {
         // プレビュー
-        CameraPreview(lifecycleOwner = lifecycleOwner, modifier = Modifier.fillMaxWidth())
+        AndroidView(
+            modifier = modifier
+                .fillMaxWidth()
+                .aspectRatio(3.0f / 4.0f),
+            factory = { context ->
+
+                // プレビュー
+                // https://developer.android.com/training/camerax/preview?hl=ja
+                val scaleType = PreviewView.ScaleType.FILL_CENTER
+
+                val previewView = PreviewView(context).apply {
+                    this.scaleType = scaleType
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                    )
+                    implementationMode = PreviewView.ImplementationMode.PERFORMANCE
+                }
+
+                val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+
+                cameraProviderFuture.addListener({
+                    val cameraProvider = cameraProviderFuture.get()
+
+                    val preview: Preview = Preview.Builder()
+                        .build()
+
+                    val cameraSelector: CameraSelector = CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build()
+
+                    preview.setSurfaceProvider(previewView.surfaceProvider)
+
+                    try {
+                        cameraProvider.unbindAll()
+                        cameraProvider.bindToLifecycle(
+                            lifecycleOwner,
+                            cameraSelector,
+                            imageCapture,
+                            preview,
+                        )
+                    } catch (e: Exception) {
+                        Log.e("CameraScreen", "Use case binding failed", e)
+                    }
+
+                }, ContextCompat.getMainExecutor(context))
+
+                previewView
+            },
+        )
 
         ConstraintLayout(
             modifier = Modifier.fillMaxSize()
@@ -35,6 +106,23 @@ fun CameraScreen(
                     .constrainAs(shutterButton) {
                         centerTo(parent)
                     },
+                onClick = {
+                    val outputOptions = ImageCapture.OutputFileOptions
+                        .Builder(createImageFile(context))
+                        .build()
+
+                    imageCapture.takePicture(outputOptions,
+                        ContextCompat.getMainExecutor(context),
+                        object : ImageCapture.OnImageSavedCallback {
+                            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                                Log.d("CameraScreen", "Save a picture: Success")
+                            }
+
+                            override fun onError(exception: ImageCaptureException) {
+                                Log.e("CameraScreen", "Save a picture: Error", exception)
+                            }
+                        })
+                }
             )
             SwitchCameraButton(
                 modifier = Modifier
@@ -47,4 +135,13 @@ fun CameraScreen(
             )
         }
     }
+}
+
+private fun createImageFile(context: Context): File {
+    val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    return File.createTempFile(
+        "image", /* prefix */
+        ".jpg", /* suffix */
+        storageDir /* directory */
+    )
 }
